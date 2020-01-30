62e87ef35f0800218c2628f16df2b93409f5a49d
TOSS <tiago.medice@gft.com>
2020-01-30 10:42:34 +0100

    ref

---
+++ kafka-create-topics.bash
@@ -0,0 +1,23 @@
+  
+#!/bin/bash
+
+# script: kafka-create-topics.sh
+# script description: creates kafka topics
+# build Stage: Alpha
+# Author: tiago medice (@a73s)
+
+
+
+# *********** Creating Kafka Topics**************
+#Reference: split string into an array in bash
+source ./.env
+source ./config
+
+IFS=', ' read -r -a topics <<< "$KAFKA_CREATE_TOPICS"
+
+for t in ${topics[@]}; do 
+  echo "[INSTALLATION-INFO] Creating Kafka Topic : ${t} "
+  ${KAFKA_HOME}/bin/kafka-topics.sh --create --zookeeper ${ZOOKEEPER_NAME}:2181 --replication-factor ${REPLICATION_FACTOR} --partitions ${PARTITIONS} --topic ${t} --if-not-exists
+done
+
+wait
